768c12e4aa460bbb6a006ae9164aba3f64d4e147
Florian Thiel <flth@flth.de>
2023-03-04 22:24:22 +0100

    beta release

---
+++ mod.lua
@@ -0,0 +1,25 @@
+function data()
+return {
+    info = {
+      minorVersion = 0,
+      severityAdd = "NONE",
+      severityRemove = "WARNING",
+      name = _("mod_name"),
+      description = _("mod_desc"),
+      authors = {
+	    {
+          name = "Modwerkstatt",
+          role = "CREATOR",
+        },
+      },
+      tags = {"waggon", "drg", "kpev",  "preußen", "personenwagen" },
+      visible = true,
+      params = {
+      },
+    },
+ }
+end
+
+
+
+		
---
+++ strings.lua
@@ -0,0 +1,142 @@
+function data()
+return {
+		de = {
+
+			["mod_name"] = "Preußische Durchgangswagen 1897",
+			["mod_desc"] = "Durchgangswagen sind Personenwagen mit einem Mittel- oder Seitengang. Der Einstieg erfolgt in der Regel über die offenen oder geschlossenen Endbühnen. Diese Modelle entsprachen der Bauserie ab 1897 und wurden bis ca. 1945 eingesetzt, ab 1928 ohne die 4. Klasse.",
+			
+            ["prdgw96d_menu"] = "Preußische Durchgangswagen 1897",
+			["prdgw96_desc"] = "Durchgangswagen sind Personenwagen mit einem Mittel- oder Seitengang. Der Einstieg erfolgt in der Regel über die offenen oder geschlossenen Endbühnen. Diese Modelle entsprachen der Bauserie ab 1897 und wurden bis ca. 1945 eingesetzt, ab 1928 ohne die 4. Klasse.",
+			
+			["ab1_name"] = "KPEV Klasse 1/2 geschlossen (1896)",
+			["ab1_10_name"] = "KPEV Klasse 1/2 geschlossen (1910)",
+			["ab1_14_name"] = "KPEV Klasse 1/2 geschlossen (1914)",
+			["ab1_drg_name"] = "DRG Klasse 1/2 geschlossen (1923)",
+			
+			["ab2_name"] = "KPEV Klasse 1/2 offen (1896)",
+			["ab2_10_name"] = "KPEV Klasse 1/2 offen (1910)",
+			["ab2_14_name"] = "KPEV Klasse 1/2 offen (1914)",
+			["ab2_drg_name"] = "DRG Klasse 1/2 offen (1923)",
+			
+			["ab3_name"] = "KPEV Klasse 1/2 einseitig offen (1896)",
+			["ab3_10_name"] = "KPEV Klasse 1/2 einseitig offen (1910)",
+			["ab3_14_name"] = "KPEV Klasse 1/2 einseitig offen (1914)",
+			["ab3_drg_name"] = "DRG Klasse 1/2 einseitig offen (1923)",
+			
+			["cd1_name"] = "KPEV Klasse 3/4 geschlossen (1896)",
+			["cd1_10_name"] = "KPEV Klasse 3/4 geschlossen (1910)",
+			["cd1_14_name"] = "KPEV Klasse 3/4 geschlossen (1914)",
+			["cd1_drg_name"] = "DRG Klasse 3/4 geschlossen (1923)",
+			
+			["cd2_name"] = "KPEV Klasse 3/4 offen (1896)",
+			["cd2_10_name"] = "KPEV Klasse 3/4 offen (1910)",
+			["cd2_14_name"] = "KPEV Klasse 3/4 offen (1914)",
+			["cd2_drg_name"] = "DRG Klasse 3/4 offen (1923)",
+			
+			["cd3_name"] = "KPEV Klasse 3/4 einseitig offen (1896)",
+			["cd3_10_name"] = "KPEV Klasse 3/4 einseitig offen (1910)",
+			["cd3_14_name"] = "KPEV Klasse 3/4 einseitig offen (1914)",
+			["cd3_drg_name"] = "DRG Klasse 3/4 einseitig offen (1923)",
+			
+			["c1_name"] = "KPEV Klasse 3 geschlossen (1896)",
+			["c1_10_name"] = "KPEV Klasse 3 geschlossen (1910)",
+			["c1_14_name"] = "KPEV Klasse 3 geschlossen (1914)",
+			["c1_drg_name"] = "DRG Klasse 3 geschlossen (1923)",
+			
+			["c2_name"] = "KPEV Klasse 3 offen (1896)",
+			["c2_10_name"] = "KPEV Klasse 3 offen (1910)",
+			["c2_14_name"] = "KPEV Klasse 3 offen (1914)",
+			["c2_drg_name"] = "DRG Klasse 3 offen (1923)",
+			
+			["c3_name"] = "KPEV Klasse 3 einseitig offen (1896)",
+			["c3_10_name"] = "KPEV Klasse 3 einseitig offen (1910)",
+			["c3_14_name"] = "KPEV Klasse 3 einseitig offen (1914)",
+			["c3_drg_name"] = "DRG Klasse 3 einseitig offen (1923)",
+			
+			["d1_name"] = "KPEV Klasse 4 geschlossen (1896)",
+			["d1_10_name"] = "KPEV Klasse 4 geschlossen (1910)",
+			["d1_14_name"] = "KPEV Klasse 4 geschlossen (1914)",
+			["d1_drg_name"] = "DRG Klasse 4 geschlossen (1923)",
+			
+			["d2_name"] = "KPEV Klasse 4 offen (1896)",
+			["d2_10_name"] = "KPEV Klasse 4 offen (1910)",
+			["d2_14_name"] = "KPEV Klasse 4 offen (1914)",
+			["d2_drg_name"] = "DRG Klasse 4 offen (1923)",
+			
+			["d3_name"] = "KPEV Klasse 4 einseitig offen (1896)",
+			["d3_10_name"] = "KPEV Klasse 4 einseitig offen (1910)",
+			["d3_14_name"] = "KPEV Klasse 4 einseitig offen (1914)",
+			["d3_drg_name"] = "DRG Klasse 4 einseitig offen (1923)",
+			
+		},
+		en = {
+
+			["mod_name"] = "Prussian Through Coaches 1897",
+			["mod_desc"] = "Through coaches are passenger coaches with a central or side aisle. Entry is usually via the open or closed end platforms. These models corresponded to the construction series from 1897 and were used until approx. 1945, from 1928 without the 4th class.",
+			
+            ["prdgw96d_menu"] = "Prussian Through Coaches 1897",
+			["prdgw96_desc"] = "Through coaches are passenger coaches with a central or side aisle. Entry is usually via the open or closed end platforms. These models corresponded to the construction series from 1897 and were used until approx. 1945, from 1928 without the 4th class.",
+			
+			["ab1_name"] = "KPEV Class 1/2 closed (1896)",
+			["ab1_10_name"] = "KPEV Class 1/2 closed (1910)",
+			["ab1_14_name"] = "KPEV Class 1/2 closed (1914)",
+			["ab1_drg_name"] = "DRG Class 1/2 closed (1923)",
+			
+			["ab2_name"] = "KPEV Class 1/2 open (1896)",
+			["ab2_10_name"] = "KPEV Class 1/2 open (1910)",
+			["ab2_14_name"] = "KPEV Class 1/2 open (1914)",
+			["ab2_drg_name"] = "DRG Class 1/2 open (1923)",
+			
+			["ab3_name"] = "KPEV Class 1/2 one-sided open (1896)",
+			["ab3_10_name"] = "KPEV Class 1/2 one-sided open (1910)",
+			["ab3_14_name"] = "KPEV Class 1/2 one-sided open (1914)",
+			["ab3_drg_name"] = "DRG Class 1/2 one-sided open (1923)",
+			
+			["cd1_name"] = "KPEV Class 3/4 closed (1896)",
+			["cd1_10_name"] = "KPEV Class 3/4 closed (1910)",
+			["cd1_14_name"] = "KPEV Class 3/4 closed (1914)",
+			["cd1_drg_name"] = "DRG Class 3/4 closed (1923)",
+			
+			["cd2_name"] = "KPEV Class 3/4 open (1896)",
+			["cd2_10_name"] = "KPEV Class 3/4 open (1910)",
+			["cd2_14_name"] = "KPEV Class 3/4 open (1914)",
+			["cd2_drg_name"] = "DRG Class 3/4 open (1923)",
+			
+			["cd3_name"] = "KPEV Class 3/4 one-sided open (1896)",
+			["cd3_10_name"] = "KPEV Class 3/4 one-sided open (1910)",
+			["cd3_14_name"] = "KPEV Class 3/4 one-sided open (1914)",
+			["cd3_drg_name"] = "DRG Class 3/4 one-sided open (1923)",
+			
+			["c1_name"] = "KPEV Class 3 closed (1896)",
+			["c1_10_name"] = "KPEV Class 3 closed (1910)",
+			["c1_14_name"] = "KPEV Class 3 closed (1914)",
+			["c1_drg_name"] = "DRG Class 3 closed (1923)",
+			
+			["c2_name"] = "KPEV Class 3 open (1896)",
+			["c2_10_name"] = "KPEV Class 3 open (1910)",
+			["c2_14_name"] = "KPEV Class 3 open (1914)",
+			["c2_drg_name"] = "DRG Class 3 open (1923)",
+			
+			["c3_name"] = "KPEV Class 3 one-sided open (1896)",
+			["c3_10_name"] = "KPEV Class 3 one-sided open (1910)",
+			["c3_14_name"] = "KPEV Class 3 one-sided open (1914)",
+			["c3_drg_name"] = "DRG Class 3 one-sided open (1923)",
+			
+			["d1_name"] = "KPEV Class 4 closed (1896)",
+			["d1_10_name"] = "KPEV Class 4 closed (1910)",
+			["d1_14_name"] = "KPEV Class 4 closed (1914)",
+			["d1_drg_name"] = "DRG Class 4 closed (1923)",
+			
+			["d2_name"] = "KPEV Class 4 open (1896)",
+			["d2_10_name"] = "KPEV Class 4 open (1910)",
+			["d2_14_name"] = "KPEV Class 4 open (1914)",
+			["d2_drg_name"] = "DRG Class 4 open (1923)",
+			
+			["d3_name"] = "KPEV Class 4 one-sided open (1896)",
+			["d3_10_name"] = "KPEV Class 4 one-sided open (1910)",
+			["d3_14_name"] = "KPEV Class 4 one-sided open (1914)",
+			["d3_drg_name"] = "DRG Class 4 one-sided open (1923)",
+			
+		},
+	}
+end
